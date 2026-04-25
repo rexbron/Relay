d6466edf6a40ee81a74ed9823fbe8227c388bc75
Link Dupont <link@sub-pop.net>
2026-04-25 06:59:02 -0400

    Use correct quotes in ci_script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cp "${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig.example ${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig"
+cp "${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig.example" "${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig"
 sed -i -e "s/your_team_id_here/${CI_TEAM_ID}/" "${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig"
 sed -i -e "s/your_giphy_api_key_here/${GIPHY_API_KEY}/" "${CI_PRIMARY_REPOSITORY_PATH}/Secrets.xcconfig"
